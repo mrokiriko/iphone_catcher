252fa404872379cf0d217abe9f94583f65c169d2
mrokiriko <4766067-mrokiriko@users.noreply.gitlab.com>
2022-10-07 12:17:08 +0400

    cool output

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import os
 from dotenv import load_dotenv
 import traceback
+import json
 
 load_dotenv()
 
@@ -29,17 +30,75 @@ def save_to_file(text):
         f.write(text)
 
 
+iphones = {
+    "MQ1W3AA/A": "iPhone 14 Pro 512GB Silver",
+    "MQ2G3AA/A": "iPhone 14 Pro 1TB Space Black",
+    "MQAJ3AA/A": "iPhone 14 Pro Max 512GB Gold",
+    "MQ0T3AA/A": "iPhone 14 Pro 256GB Space Black",
+    "MQ9V3AA/A": "iPhone 14 Pro Max 256GB Silver",
+    "MPXV3AA/A": "iPhone 14 Pro 128GB Space Black",
+    "MQ2V3AA/A": "iPhone 14 Pro 1TB Gold",
+    "MQ9W3AA/A": "iPhone 14 Pro Max 256GB Gold",
+    "MQ323AA/A": "iPhone 14 Pro 1TB Deep Purple",
+    "MQ2N3AA/A": "iPhone 14 Pro 1TB Silver",
+    "MQ023AA/A": "iPhone 14 Pro 128GB Silver",
+    "MQ9P3AA/A": "iPhone 14 Pro Max 128GB Space Black",
+    "MQ293AA/A": "iPhone 14 Pro 512GB Deep Purple",
+    "MQAM3AA/A": "iPhone 14 Pro Max 512GB Deep Purple",
+    "MQ9U3AA/A": "iPhone 14 Pro Max 256GB Space Black",
+    "MQ1F3AA/A": "iPhone 14 Pro 256GB Deep Purple",
+    "MQC33AA/A": "iPhone 14 Pro Max 1TB Silver",
+    "MQC53AA/A": "iPhone 14 Pro Max 1TB Deep Purple",
+    "MQ9R3AA/A": "iPhone 14 Pro Max 128GB Gold",
+    "MQAH3AA/A": "iPhone 14 Pro Max 512GB Silver",
+    "MQC23AA/A": "iPhone 14 Pro Max 1TB Space Black",
+    "MQ9X3AA/A": "iPhone 14 Pro Max 256GB Deep Purple",
+    "MQ233AA/A": "iPhone 14 Pro 512GB Gold",
+    "MQ0G3AA/A": "iPhone 14 Pro 128GB Deep Purple",
+    "MQ083AA/A": "iPhone 14 Pro 128GB Gold",
+    "MQ9Q3AA/A": "iPhone 14 Pro Max 128GB Silver",
+    "MQ9T3AA/A": "iPhone 14 Pro Max 128GB Deep Purple",
+    "MQAF3AA/A": "iPhone 14 Pro Max 512GB Space Black",
+    "MQ103AA/A": "iPhone 14 Pro 256GB Silver",
+    "MQ1M3AA/A": "iPhone 14 Pro 512GB Space Black",
+    "MQC43AA/A": "iPhone 14 Pro Max 1TB Gold",
+    "MQ183AA/A": "iPhone 14 Pro 256GB Gold",
+}
+
+
+store_names = {
+    "R597": "Dubai / Dubai Mall",
+    "R596": "Dubai / Mall of the Emirates",
+    "R595": "Abu Dhabi / Yas Mall",
+    "R706": "Abu Dhabi / Al Maryah Island",
+}
+
+
+def availability_message(stores, store_code):
+    message = ""
+    store = stores[store_code]
+    for iphone in store:
+        if store[iphone]["availability"]["unlocked"]:
+            message += iphones[iphone] + " in " + store_names[store_code] + "\n"
+    return message
+
+
 def foo():
     contents = urllib.request.urlopen(availability_url).read()
     content = contents.decode("utf8")
     save_to_file(content)
-    # if "We’re not taking reservations to buy iPhone in the store right now." in content:
-    # if "true" in content:
-    if content.count("true") > 1:
-        printer('GOT IT')
-        r = requests.post(ifttt_url, json={
-            "this": 1
-        })
+
+    json_obj = json.loads(content)
+
+    store1 = availability_message(json_obj["stores"], "R597")
+    store2 = availability_message(json_obj["stores"], "R596")
+
+    store_msg = store1 + store2
+
+    # if content.count("true") > 1:
+    if len(store_msg) > 0:
+        printer(store_msg)
+        r = requests.post(ifttt_url, json={"msg": store_msg})
     else:
         printer('not got it')
 
